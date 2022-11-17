7b803ccdc291c1467bcf2fa9d0a3d3c9605e51bd
Adeope63 <adeope63@gmail.com>
2022-11-17 23:42:03 +0400

    Adding The Json file to get variables from

---
+++ lessonData.js
@@ -0,0 +1,82 @@
+let lesson =[
+    {
+        "id": 1,
+        "subject": "Math",
+        "location": "London",
+        "price": 100,
+        "space": 5,
+        "imgURL" : "./assets/math.png"
+    },
+    {
+        "id": 2,
+        "subject": "Music",
+        "location": "America",
+        "price": 300,
+        "space": 5,
+        "imgURL" : "./assets/music.png"
+    },
+    {
+        "id": 3,
+        "subject": "English",
+        "location": "America",
+        "price": 500,
+        "space": 5,
+        "imgURL" : "./assets/english.png"
+    },
+    {
+        "id": 4,
+        "subject": "Dance",
+        "location": "America",
+        "price": 600,
+        "space": 5,
+        "imgURL" : "./assets/dance.png"
+    },
+    {
+        "id": 5,
+        "subject": "Computer",
+        "location": "America",
+        "price": 600,
+        "space": 5,
+        "imgURL" : "./assets/computer.png"
+    },
+    {
+        "id": 6,
+        "subject": "Math",
+        "location": "America",
+        "price": 900,
+        "space": 5,
+        "imgURL" : "./assets/math.png"
+    },
+    {
+        "id": 7,
+        "subject": "Music",
+        "location": "London",
+        "price": 300,
+        "space": 5,
+        "imgURL" : "./assets/music.png"
+    },
+    {
+        "id": 8,
+        "subject": "English",
+        "location": "London",
+        "price": 900,
+        "space": 5,
+        "imgURL" : "./assets/english.png"
+    },
+    {
+        "id": 9,
+        "subject": "Dance",
+        "location": "London",
+        "price": 200,
+        "space": 5,
+        "imgURL" : "./assets/dance.png"
+    },
+    {
+        "id": 10,
+        "subject": "Computer",
+        "location": "America",
+        "price": 300,
+        "space": 5,
+        "imgURL" : "./assets/computer.png"
+    }
+]
